6cedb250c9ee9832e8dc740802a366e69d16a554
asimahmad1 <>
2021-04-05 10:59:15 +0000

    O(1) Complexity

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-const nemo = ['Nemo'];
-const everyone = ['Asim', 'Nitish', 'Aakash' 'Ankit'];
-const large = new Array(100).fill('Nemo');
+// const nemo = ['Nemo'];
+// const everyone = ['Asim', 'Nitish', 'Aakash', 'Ankit'];
+// const large = new Array(10000).fill('Nemo');
 
-function findNemo(array)
+// function findNemo(array)
+// {
+//   let t0= performance.now();
+//   for (let i=0; i<array.length; i++)
+//   {
+//     if(array[i] === 'Nemo')
+//     {
+//       console.log('Found nemo');
+//     }
+//     else
+//     {
+//       console.log('Not found');
+//     }
+//   }
+//   let t1= performance.now();
+//   console.log('Time: '+ (t1-t0));
+// }
+
+// findNemo(large);
+// //findNemo(everyone);
+const boxes = [0,1,2,3,4,5];
+
+function logFirstTwoBoxes(boxes)
 {
-  let t0= performance.now();
-  for (let i=0; i<array.length; i++)
-  {
-    if(array[i] === 'Nemo')
-    {
-      console.log('Found nemo');
-    }
-    else
-    {
-      console.log('Not found');
-    }
-  }
-  let t1= performance.now();
-  console.log('Time: '+ (t1-t0));
+  console.log(boxes[0]);
+  console.log(boxes[1]);
 }
-
-findNemo(large);
-//findNemo(everyone);
-
+logFirstTwoBoxes(boxes);
