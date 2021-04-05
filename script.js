c07b90c23cff0589924a067160c35d0ec85e9598
Asim <>
2021-04-05 06:55:28 +0000

    Added the time function to check the complexity.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const nemo = ['Nemo'];
+const everyone = ['Asim', 'Nitish', 'Aakash' 'Ankit'];
+const large = new Array(100).fill('Nemo');
+
 function findNemo(array)
 {
+  let t0= performance.now();
   for (let i=0; i<array.length; i++)
   {
     if(array[i] === 'Nemo')
@@ -12,5 +16,10 @@ function findNemo(array)
       console.log('Not found');
     }
   }
+  let t1= performance.now();
+  console.log('Time: '+ (t1-t0));
 }
+
+findNemo(large);
+//findNemo(everyone);
 
