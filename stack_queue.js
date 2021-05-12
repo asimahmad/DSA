e49b607e5748a156b34ee73ee819593d7c5ba6e0
Asim <>
2021-05-12 06:20:12 +0000

    Queue with linkedlist.

--- stack_queue.js
+++ stack_queue.js
@@ -57,32 +57,95 @@
 
 
 
-// if stack with array
+// // if stack with array
 
 
-class Stack{
-  constructor(){
-    this.array = [];
-  }
+// class Stack{
+//   constructor(){
+//     this.array = [];
+//   }
 
-  peek(){
-    return this.array[this.array.length-1];
-  }
+//   peek(){
+//     return this.array[this.array.length-1];
+//   }
 
-  push(value){
-    this.array.push(value);
-    return this;
-  }
+//   push(value){
+//     this.array.push(value);
+//     return this;
+//   }
 
-  pop(){
-    this.array.pop();
-    return this.array;
+//   pop(){
+//     this.array.pop();
+//     return this.array;
+//   }
+// }
+
+// const myStack = new Stack();
+// myStack.push("Google");
+// myStack.push("Udemy");
+// myStack.push("Discord");
+// myStack.peek();
+// myStack.pop();
+
+
+
+
+
+// Queue
+
+
+class Node{
+  constructor(value){
+    this.vaue = value;
+    this.next = null;
   }
 }
 
-const myStack = new Stack();
-myStack.push("Google");
-myStack.push("Udemy");
-myStack.push("Discord");
-myStack.peek();
-myStack.pop();
+
+class Queue{
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek(){
+    return this.first;
+  }
+  
+  enqueue(value){
+    const newNode = new Node(value);
+    if(this.length == 0){
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  dequeue(){
+    if(!this.first){
+      return null;
+    }
+    if(this.first === this.last){
+      this.last = null;
+    }
+    const holidingPointer = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+}
+
+
+const myQueue = new Queue();
+myQueue.peek();
+myQueue.enqueue("Joy");
+myQueue.enqueue("Matt");
+myQueue.enqueue("Pavel");
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
